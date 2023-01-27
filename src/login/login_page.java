6734af659e0c9716b0dda0329d9aaed0dcf34c01
Sweety <Sweety@Nammi>
2023-01-27 08:31:43 +0530

    Friday First college

---
+++ src/login/login_page.java
@@ -0,0 +1,17 @@
+package login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class login_page {
+
+	public static void main(String[] args) {
+	/*	System.setProperty("webdriver.gecko.driver", "C:\\selenium\\drivers\\geckodriver.exe");
+        WebDriver driver =new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        */
+        
+        
+	}
+
+}
